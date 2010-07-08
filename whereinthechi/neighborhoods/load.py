06315a74f1bcea15f8f3d4d89443fa6499c39e13
ghing <ghing@geoffsnotebook>
2010-07-08 01:12:10 -0500

    Tried to get test cases working
    Currently the tests fail because a call to add_postgis_srs(102671) needs to happen before the model's 
    database tables get created.  I don't know how to do this with Django's unit test framework.  I asked for 
    help at http://stackoverflow.com/questions/3200524/making-a-call-to-add-postgis-srs-before-geodjango-model-tables-get-created

--- whereinthechi/neighborhoods/load.py
+++ whereinthechi/neighborhoods/load.py
@@ -1,19 +1,19 @@
 import os
-from django.contrib.gis.utils import LayerMapping
+#from django.core import management
+from django.contrib.gis.utils import LayerMapping, add_postgis_srs
 from models import Neighborhoods, neighborhoods_mapping
-
-# These use unusual Spatial Reference System
-# NAD 1983 StatePlane Illinois East FIPS 1201 Feet
-# http://spatialreference.org/ref/esri/102671/
-# This needs to be done before syncdb
-#        add_postgis_srs(102671)
-        
-#management.call_command('syncdb')
-
 
 neighborhoods_shp = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data/Neighboorhoods.shp'))
 
 def run(verbose=True):
+    # These use unusual Spatial Reference System
+    # NAD 1983 StatePlane Illinois East FIPS 1201 Feet
+    # http://spatialreference.org/ref/esri/102671/
+    # This needs to be done before syncdb
+    #add_postgis_srs(102671)
+    #management.call_command('reset', 'neighborhoods')
+
+
     lm = LayerMapping(Neighborhoods,  neighborhoods_shp, neighborhoods_mapping,
                       transform=False, encoding='iso-8859-1')
 
--- whereinthechi/neighborhoods/tests.py
+++ whereinthechi/neighborhoods/tests.py
@@ -2,7 +2,18 @@ from django.test import TestCase
 
 from neighborhoods.models import Neighborhoods
 from django.contrib.gis.geos import Point
+
+#from django.core import management
+#from django.contrib.gis.utils import add_postgis_srs
+
+#add_postgis_srs(102671)
+
 class SimpleTest(TestCase):
+    def setUp(self):
+        #add_postgis_srs(102671)
+        #management.call_command('reset', 'neighborhoods')
+        pass
+        
     def test_my_neighborhood(self):
         pnt = Point(-87.643078, 41.938840, srid=4326)
         #pnt_wkt = 'POINT(38.5847 56.3304)'
@@ -10,6 +21,3 @@ class SimpleTest(TestCase):
         qs = Neighborhoods.objects.filter(geom__intersects=pnt)
         print qs
         self.assertNotEqual(0, len(qs))
-
-
-
